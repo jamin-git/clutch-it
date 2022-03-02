4b024b29fd81a49014549780ecd0039b2b48827a
Sushant Guha <s.guha@mail.utoronto.ca>
2022-03-01 19:21:43 -0500

    Navbar and Map components

--- clutch-it-app/src/App.js
+++ clutch-it-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import NavbarComponent from "./Components/NavbarComponent";
+import "./App.css";
+import MapsComponent from "./Components/MapsComponent";
+import { Container, Row, Col } from "react-bootstrap";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="vh-100"
+      style={{ backgroundColor: "#2c193f", color: "white" }}
+    >
+      <NavbarComponent />
+      <Container className="container">
+        <Row className="row" style={{ marginTop: "5%" }}>
+          {/* Centering this shit. */}
+          <Col className="col-12" style={{ marginLeft: "0%" }}>
+            <MapsComponent />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
---
+++ clutch-it-app/src/Components/MapsComponent.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import havenMap from "./haven_map.jpg";
+
+const MapsComponent = () => {
+  return (
+    <div>
+      <Card
+        className="text-center"
+        border="primary"
+        style={{
+          backgroundColor: "#2c193f",
+          width: "70%",
+          height: "100%",
+          borderRadius: "8%",
+          borderWidth: "thick",
+        }}
+      >
+        <img
+          border="primary"
+          src={havenMap}
+          style={{ width: "100%", height: "100%", borderRadius: "8%" }}
+          alt="Map Banner"
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default MapsComponent;
