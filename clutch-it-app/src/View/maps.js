8851abf706f3a1e9673db214b018d569cb5d3fd6
Sushant Guha <sush.guha@gmail.com>
2022-03-13 19:24:33 -0400

    Addition of Haven map

--- clutch-it-app/src/View/maps.js
+++ clutch-it-app/src/View/maps.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import "./maps.css";
+import { Container, Row } from "react-bootstrap";
+import havenMap from "../Resources/haven_map.jpg";
 
 const maps = () => {
   return (
-    <div>maps</div>
-  )
-}
+    <Container
+      className="maps"
+      style={{ minHeight: "100vh", minWidth: "100%" }}
+    >
+      {/* Title Row */}
+      <Row style={{ maxWidth: "50vh" }}>
+        <p className="title1" style={{ marginTop: "1%" }}>
+          Haven
+        </p>
+      </Row>{" "}
+      {/* Map Image Row */}
+      <Row className="justify-content-center">
+        <div style={{ paddingLeft: "15%" }}>
+          <img
+            className="square"
+            id="maps-frame"
+            src={havenMap}
+            alt="Haven Map"
+            style={{
+              paddingLeft: "0.1%",
+              paddingRight: "0.1%",
+              paddingTop: "0.1%",
+              paddingBottom: "0.1%",
+            }}
+          />
+        </div>
+      </Row>{" "}
+      {/* Info Box Row */}
+      <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
+        <div className="square" id="info-box">
+          <p style={{ color: "white", margin: "20%", fontSize: "1em" }}>
+            3 Sites
+          </p>
+        </div>
+        {/* <div
+          className="square"
+          id="info-box"
+          style={{ marginLeft: "4%" }}
+        ></div> */}
+      </Row>{" "}
+    </Container>
+  );
+};
 
-export default maps
+export default maps;
