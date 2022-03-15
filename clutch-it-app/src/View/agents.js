b02ff33c05fc5776abe234a875cb1311a94f3d85
andyng2002 <andyng2002@gmail.com>
2022-03-15 16:44:39 -0400

    updated agents page

--- clutch-it-app/src/View/agents.js
+++ clutch-it-app/src/View/agents.js
@@ -10,19 +10,20 @@ const agents = () => {
   return (
     <div className='agents'>
       <div>
-        <h1 className="title1"> <u>AGENT TYPES</u></h1>
+        <h1 className="title"> <u>AGENT TYPES</u></h1>
       </div>
-        <h2 className="controller">CONTROLLER </h2>
-        
-        <img className='image' src={controller} style={{}}/>
-        
-        <img className='image' src={sentinel} style={{}}/>
-
-      <div>
+      <div class="flexbox-container">
+        <h2 className="controller">CONTROLLER</h2>
+        <h2 className="sentinel">SENTINEL</h2>
+      </div>
+      <img className='image' src={controller} style={{}}/>
+      <img className='image' src={sentinel} style={{}}/>
+      <div class="flexbox-container">
         <h2 className="initiator">INITIATOR</h2>
-        <img className='image' src={initiator} style={{}}/>
-        <img className='image' src={duelist} style={{}}/>
+        <h2 className="duelist">DUELIST</h2>
       </div>
+      <img className='image' src={initiator} style={{}}/>
+      <img className='image' src={duelist} style={{}}/>
     </div>
   )
 }
