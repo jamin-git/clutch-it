c5ba93bbc408728ec7e06546a22473fd1e828811
Sushant Guha <sush.guha@gmail.com>
2022-03-15 17:00:44 -0400

    Maps-frames-complete

--- clutch-it-app/src/View/maps.js
+++ clutch-it-app/src/View/maps.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./maps.css";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+
+// Importing map images
 import havenMap from "../Resources/haven_map.jpg";
+import ascentMap from "../Resources/ascent_map.jpg";
+import iceboxMap from "../Resources/icebox_map.jpg";
+import bindMap from "../Resources/bind_map.jpg";
+import breezeMap from "../Resources/breeze_map.jpg";
+import splitMap from "../Resources/split_map.jpg";
+import fractureMap from "../Resources/fracture_map.jpg";
 
 const maps = () => {
   return (
@@ -9,6 +17,7 @@ const maps = () => {
       className="maps"
       style={{ minHeight: "100vh", minWidth: "100%" }}
     >
+      {/* Map-1 : Haven */}
       {/* Title Row */}
       <Row style={{ maxWidth: "50vh" }}>
         <p className="title1" style={{ marginTop: "1%" }}>
@@ -45,6 +54,529 @@ const maps = () => {
           style={{ marginLeft: "4%" }}
         ></div> */}
       </Row>{" "}
+      {/* Videos */}
+      <Row>
+        <Col style={{ marginLeft: "9%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "22%" }}>
+            TRENDING
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={havenMap}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+
+        <Col style={{ marginLeft: "16.4%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "21%" }}>
+            CLUTCHED
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={havenMap}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+      </Row>
+      {/* Next Map ---------------------------------------------------*/}
+      {/* Map-2 : Ascent */}
+      {/* Title Row */}
+      <Row style={{ maxWidth: "50vh" }}>
+        <p className="title1" style={{ marginTop: "50%" }}>
+          Ascent
+        </p>
+      </Row>{" "}
+      {/* Map Image Row */}
+      <Row className="justify-content-center">
+        <div style={{ paddingLeft: "15%" }}>
+          <img
+            className="square"
+            id="maps-frame"
+            src={ascentMap}
+            alt="Ascent Map"
+            style={{
+              paddingLeft: "0.1%",
+              paddingRight: "0.1%",
+              paddingTop: "0.1%",
+              paddingBottom: "0.1%",
+            }}
+          />
+        </div>
+      </Row>{" "}
+      {/* Info Box Row */}
+      <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
+        <div className="square" id="info-box">
+          <p style={{ color: "white", margin: "20%", fontSize: "1em" }}>
+            2 Sites
+          </p>
+        </div>
+        {/* <div
+          className="square"
+          id="info-box"
+          style={{ marginLeft: "4%" }}
+        ></div> */}
+      </Row>{" "}
+      {/* Videos */}
+      <Row>
+        <Col style={{ marginLeft: "9%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "22%" }}>
+            TRENDING
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={ascentMap}
+              alt="ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+
+        <Col style={{ marginLeft: "16.4%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "21%" }}>
+            CLUTCHED
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={ascentMap}
+              alt="Ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+      </Row>
+      {/* Next Map ---------------------------------------------------*/}
+      {/* Map-3 : IceBox */}
+      {/* Title Row */}
+      <Row style={{ maxWidth: "50vh" }}>
+        <p className="title1" style={{ marginTop: "50%" }}>
+          IceBox
+        </p>
+      </Row>{" "}
+      {/* Map Image Row */}
+      <Row className="justify-content-center">
+        <div style={{ paddingLeft: "15%" }}>
+          <img
+            className="square"
+            id="maps-frame"
+            src={iceboxMap}
+            alt="Ascent Map"
+            style={{
+              paddingLeft: "0.1%",
+              paddingRight: "0.1%",
+              paddingTop: "0.1%",
+              paddingBottom: "0.1%",
+            }}
+          />
+        </div>
+      </Row>{" "}
+      {/* Info Box Row */}
+      <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
+        <div className="square" id="info-box">
+          <p style={{ color: "white", margin: "20%", fontSize: "1em" }}>
+            2 Sites
+          </p>
+        </div>
+        {/* <div
+          className="square"
+          id="info-box"
+          style={{ marginLeft: "4%" }}
+        ></div> */}
+      </Row>{" "}
+      {/* Videos */}
+      <Row>
+        <Col style={{ marginLeft: "9%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "22%" }}>
+            TRENDING
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={iceboxMap}
+              alt="ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+
+        <Col style={{ marginLeft: "16.4%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "21%" }}>
+            CLUTCHED
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={iceboxMap}
+              alt="Ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+      </Row>
+      {/* Next Map ---------------------------------------------------*/}
+      {/* Map-4 : Bind */}
+      {/* Title Row */}
+      <Row style={{ maxWidth: "50vh" }}>
+        <p className="title1" style={{ marginTop: "50%" }}>
+          Bind
+        </p>
+      </Row>{" "}
+      {/* Map Image Row */}
+      <Row className="justify-content-center">
+        <div style={{ paddingLeft: "15%" }}>
+          <img
+            className="square"
+            id="maps-frame"
+            src={bindMap}
+            alt="Ascent Map"
+            style={{
+              paddingLeft: "0.1%",
+              paddingRight: "0.1%",
+              paddingTop: "0.1%",
+              paddingBottom: "0.1%",
+            }}
+          />
+        </div>
+      </Row>{" "}
+      {/* Info Box Row */}
+      <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
+        <div className="square" id="info-box">
+          <p style={{ color: "white", margin: "20%", fontSize: "1em" }}>
+            2 Sites
+          </p>
+        </div>
+        {/* <div
+          className="square"
+          id="info-box"
+          style={{ marginLeft: "4%" }}
+        ></div> */}
+      </Row>{" "}
+      {/* Videos */}
+      <Row>
+        <Col style={{ marginLeft: "9%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "22%" }}>
+            TRENDING
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={bindMap}
+              alt="ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+
+        <Col style={{ marginLeft: "16.4%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "21%" }}>
+            CLUTCHED
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={bindMap}
+              alt="Ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+      </Row>
+      {/* Next Map ---------------------------------------------------*/}
+      {/* Map-5 : Breeze */}
+      {/* Title Row */}
+      <Row style={{ maxWidth: "50vh" }}>
+        <p className="title1" style={{ marginTop: "50%" }}>
+          Breeze
+        </p>
+      </Row>{" "}
+      {/* Map Image Row */}
+      <Row className="justify-content-center">
+        <div style={{ paddingLeft: "15%" }}>
+          <img
+            className="square"
+            id="maps-frame"
+            src={breezeMap}
+            alt="Ascent Map"
+            style={{
+              paddingLeft: "0.1%",
+              paddingRight: "0.1%",
+              paddingTop: "0.1%",
+              paddingBottom: "0.1%",
+            }}
+          />
+        </div>
+      </Row>{" "}
+      {/* Info Box Row */}
+      <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
+        <div className="square" id="info-box">
+          <p style={{ color: "white", margin: "20%", fontSize: "1em" }}>
+            2 Sites
+          </p>
+        </div>
+        {/* <div
+          className="square"
+          id="info-box"
+          style={{ marginLeft: "4%" }}
+        ></div> */}
+      </Row>{" "}
+      {/* Videos */}
+      <Row>
+        <Col style={{ marginLeft: "9%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "22%" }}>
+            TRENDING
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={breezeMap}
+              alt="ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+
+        <Col style={{ marginLeft: "16.4%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "21%" }}>
+            CLUTCHED
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={breezeMap}
+              alt="Ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+      </Row>
+      {/* Next Map ---------------------------------------------------*/}
+      {/* Map-6 : Split */}
+      {/* Title Row */}
+      <Row style={{ maxWidth: "50vh" }}>
+        <p className="title1" style={{ marginTop: "50%" }}>
+          Split
+        </p>
+      </Row>{" "}
+      {/* Map Image Row */}
+      <Row className="justify-content-center">
+        <div style={{ paddingLeft: "15%" }}>
+          <img
+            className="square"
+            id="maps-frame"
+            src={splitMap}
+            alt="Ascent Map"
+            style={{
+              paddingLeft: "0.1%",
+              paddingRight: "0.1%",
+              paddingTop: "0.1%",
+              paddingBottom: "0.1%",
+            }}
+          />
+        </div>
+      </Row>{" "}
+      {/* Info Box Row */}
+      <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
+        <div className="square" id="info-box">
+          <p style={{ color: "white", margin: "20%", fontSize: "1em" }}>
+            2 Sites
+          </p>
+        </div>
+        {/* <div
+          className="square"
+          id="info-box"
+          style={{ marginLeft: "4%" }}
+        ></div> */}
+      </Row>{" "}
+      {/* Videos */}
+      <Row>
+        <Col style={{ marginLeft: "9%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "22%" }}>
+            TRENDING
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={splitMap}
+              alt="ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+
+        <Col style={{ marginLeft: "16.4%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "21%" }}>
+            CLUTCHED
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={splitMap}
+              alt="Ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+      </Row>
+      {/* Next Map ---------------------------------------------------*/}
+      {/* Map-7 : Fracture */}
+      {/* Title Row */}
+      <Row style={{ maxWidth: "50vh" }}>
+        <p className="title1" style={{ marginTop: "50%" }}>
+          Fracture
+        </p>
+      </Row>{" "}
+      {/* Map Image Row */}
+      <Row className="justify-content-center">
+        <div style={{ paddingLeft: "15%" }}>
+          <img
+            className="square"
+            id="maps-frame"
+            src={fractureMap}
+            alt="Ascent Map"
+            style={{
+              paddingLeft: "0.1%",
+              paddingRight: "0.1%",
+              paddingTop: "0.1%",
+              paddingBottom: "0.1%",
+            }}
+          />
+        </div>
+      </Row>{" "}
+      {/* Info Box Row */}
+      <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
+        <div className="square" id="info-box">
+          <p style={{ color: "white", margin: "20%", fontSize: "1em" }}>
+            2 Sites
+          </p>
+        </div>
+        {/* <div
+          className="square"
+          id="info-box"
+          style={{ marginLeft: "4%" }}
+        ></div> */}
+      </Row>{" "}
+      {/* Videos */}
+      <Row>
+        <Col style={{ marginLeft: "9%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "22%" }}>
+            TRENDING
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={fractureMap}
+              alt="ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+
+        <Col style={{ marginLeft: "16.4%" }}>
+          <h1 className="title1" style={{ marginTop: "4%", marginLeft: "21%" }}>
+            CLUTCHED
+          </h1>
+          <div style={{ paddingLeft: "15%", marginTop: "2%" }}>
+            <img
+              className="square"
+              id="video-frame"
+              src={fractureMap}
+              alt="Ascent Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </div>
+        </Col>
+      </Row>
+      <Row>hello</Row>
     </Container>
   );
 };
