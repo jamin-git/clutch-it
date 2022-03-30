5991dccf3badab930bff6e2cd95e067cd2fb1f2a
mlee692 <68256202+michelle692@users.noreply.github.com>
2022-03-29 20:15:43 -0400

    homepage finished

--- clutch-it-app/src/View/highlights.js
+++ clutch-it-app/src/View/highlights.js
@@ -1,12 +1,60 @@
 import React from 'react'
+import HomeImage from '../Resources/HomeImage.svg'
+import Agents from '../Resources/AGENTS.svg'
+import Maps from '../Resources/MAPS.svg'
 import './highlights.css'
 
 const highlights = () => {
   return (
-    <div className='highlights'>
-      <h1 className='title1'>UPDATES</h1>
-        <div className='square' id='update-item'></div>
-      <h1 className='title1'>TRENDING CLIPS</h1>
+    <div class='highlights'>
+
+      <div class='home-img'>
+        <img src={HomeImage} />
+      </div>
+
+      <div>
+        <p class='title1'>UPDATES</p>
+      </div>
+        
+      <div class='updates-box'>
+      </div>
+
+      <div>
+        <p class='title2'>TRENDING CLIPS</p>
+      </div>
+      
+      <div class='trending-box'>
+      </div>
+
+      <div class='trending-box2'>
+      </div>
+
+      <div>
+        <p class='title3'>CLUTCHED IT</p>
+      </div>
+
+      <div class='clutch-box'>
+      </div>
+
+      <div class='clutch-box2'>
+      </div>
+
+      <div class='agents-homeimg'>
+        <img src={Agents} />
+      </div>
+
+      <a href="/agents">
+        <p class='agents-home'>AGENTS</p>
+      </a>
+
+      <div class='maps-homeimg'>
+        <img src={Maps} />
+      </div>
+
+      <a href="/maps">
+        <p class='maps-home'>MAPS</p>
+      </a>
+            
     </div>
   )
 }
