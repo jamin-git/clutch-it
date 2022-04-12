077aa272b9a860aa13be2ec16c9f1bf10c250666
Sushant Guha <sush.guha@gmail.com>
2022-04-12 18:55:45 -0400

    Maps carousel

--- clutch-it-app/src/View/maps.js
+++ clutch-it-app/src/View/maps.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./maps.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
 
 // Importing map images
 import havenMap from "../Resources/map images/haven_map.jpg";
@@ -10,43 +10,69 @@ import bindMap from "../Resources/map images/bind_map.jpg";
 import breezeMap from "../Resources/map images/breeze_map.jpg";
 import splitMap from "../Resources/map images/split_map.jpg";
 import fractureMap from "../Resources/map images/fracture_map.jpg";
+import havenLayout from "../Resources/map images/havenLayout.jpg";
+import ascentLayout from "../Resources/map images/ascentLayout.jpg";
+import bindLayout from "../Resources/map images/bindLayout.jpg";
+import breezeLayout from "../Resources/map images/breezeLayout.jpg";
+import splitLayout from "../Resources/map images/splitLayout.jpg";
 
 const maps = () => {
   return (
     <Container
       className="maps"
-      style={{ minHeight: "100vh", minWidth: "100%" }}
+      // style={{ minHeight: "100vh", minWidth: "100%" }}
     >
       {/* Map-1 : Haven */}
       {/* Title Row */}
       <Row style={{ maxWidth: "50vh" }}>
-        <p className="title1" style={{ marginTop: "1%" }}>
+        <p className="title1" style={{ marginTop: "5px" }}>
           Haven
         </p>
       </Row>{" "}
       {/* Map Image Row */}
-      <Row className="justify-content-center">
-        <div>
-          <img
-            className="square"
-            id="maps-frame"
-            src={havenMap}
-            alt="Haven Map"
-            style={{
-              paddingLeft: "0.1%",
-              paddingRight: "0.1%",
-              paddingTop: "0.1%",
-              paddingBottom: "0.1%",
-            }}
-          />
-        </div>
-      </Row>{" "}
+      <p style={{ marginBottom: "-27%" }}>`</p>
+      <Row className="justify-content-center" style={{ marginBottom: "24%" }}>
+        <Carousel
+          className="Carousel"
+          style={{
+            marginTop: "0px",
+            marginBottom: "0px",
+          }}
+        >
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={havenMap}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={havenLayout}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Row>
       {/* Info Box Row */}
       <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
         <div className="square" id="info-box">
-          <p style={{ color: "white", margin: "20%", fontSize: "0.8em" }}>
-            3 Sites
-          </p>
+          <p style={{ margin: "20%", fontSize: "0.8em" }}>3 Sites</p>
         </div>
         {/* <div
           className="square"
@@ -105,28 +131,49 @@ const maps = () => {
         </p>
       </Row>{" "}
       {/* Map Image Row */}
-      <Row className="justify-content-center">
-        <div>
-          <img
-            className="square"
-            id="maps-frame"
-            src={ascentMap}
-            alt="Ascent Map"
-            style={{
-              paddingLeft: "0.1%",
-              paddingRight: "0.1%",
-              paddingTop: "0.1%",
-              paddingBottom: "0.1%",
-            }}
-          />
-        </div>
-      </Row>{" "}
+      <p style={{ marginBottom: "-27%" }}>`</p>
+      <Row className="justify-content-center" style={{ marginBottom: "24%" }}>
+        <Carousel
+          className="Carousel"
+          style={{
+            marginTop: "0px",
+            marginBottom: "0px",
+          }}
+        >
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={ascentMap}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={ascentLayout}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Row>
       {/* Info Box Row */}
       <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
         <div className="square" id="info-box">
-          <p style={{ color: "white", margin: "20%", fontSize: "0.8em" }}>
-            2 Sites
-          </p>
+          <p style={{ margin: "20%", fontSize: "0.8em" }}>2 Sites</p>
         </div>
         {/* <div
           className="square"
@@ -185,28 +232,49 @@ const maps = () => {
         </p>
       </Row>{" "}
       {/* Map Image Row */}
-      <Row className="justify-content-center">
-        <div>
-          <img
-            className="square"
-            id="maps-frame"
-            src={iceboxMap}
-            alt="Ascent Map"
-            style={{
-              paddingLeft: "0.1%",
-              paddingRight: "0.1%",
-              paddingTop: "0.1%",
-              paddingBottom: "0.1%",
-            }}
-          />
-        </div>
-      </Row>{" "}
+      <p style={{ marginBottom: "-27%" }}>`</p>
+      <Row className="justify-content-center" style={{ marginBottom: "24%" }}>
+        <Carousel
+          className="Carousel"
+          style={{
+            marginTop: "0px",
+            marginBottom: "0px",
+          }}
+        >
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={iceboxMap}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={ascentLayout}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Row>
       {/* Info Box Row */}
       <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
         <div className="square" id="info-box">
-          <p style={{ color: "white", margin: "20%", fontSize: "0.8em" }}>
-            2 Sites
-          </p>
+          <p style={{ margin: "20%", fontSize: "0.8em" }}>2 Sites</p>
         </div>
         {/* <div
           className="square"
@@ -265,28 +333,49 @@ const maps = () => {
         </p>
       </Row>{" "}
       {/* Map Image Row */}
-      <Row className="justify-content-center">
-        <div>
-          <img
-            className="square"
-            id="maps-frame"
-            src={bindMap}
-            alt="Ascent Map"
-            style={{
-              paddingLeft: "0.1%",
-              paddingRight: "0.1%",
-              paddingTop: "0.1%",
-              paddingBottom: "0.1%",
-            }}
-          />
-        </div>
-      </Row>{" "}
+      <p style={{ marginBottom: "-27%" }}>`</p>
+      <Row className="justify-content-center" style={{ marginBottom: "24%" }}>
+        <Carousel
+          className="Carousel"
+          style={{
+            marginTop: "0px",
+            marginBottom: "0px",
+          }}
+        >
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={bindMap}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={bindLayout}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Row>
       {/* Info Box Row */}
       <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
         <div className="square" id="info-box">
-          <p style={{ color: "white", margin: "20%", fontSize: "0.8em" }}>
-            2 Sites
-          </p>
+          <p style={{ margin: "20%", fontSize: "0.8em" }}>2 Sites</p>
         </div>
         {/* <div
           className="square"
@@ -345,28 +434,49 @@ const maps = () => {
         </p>
       </Row>{" "}
       {/* Map Image Row */}
-      <Row className="justify-content-center">
-        <div>
-          <img
-            className="square"
-            id="maps-frame"
-            src={breezeMap}
-            alt="Ascent Map"
-            style={{
-              paddingLeft: "0.1%",
-              paddingRight: "0.1%",
-              paddingTop: "0.1%",
-              paddingBottom: "0.1%",
-            }}
-          />
-        </div>
-      </Row>{" "}
+      <p style={{ marginBottom: "-27%" }}>`</p>
+      <Row className="justify-content-center" style={{ marginBottom: "24%" }}>
+        <Carousel
+          className="Carousel"
+          style={{
+            marginTop: "0px",
+            marginBottom: "0px",
+          }}
+        >
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={breezeMap}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={breezeLayout}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Row>
       {/* Info Box Row */}
       <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
         <div className="square" id="info-box">
-          <p style={{ color: "white", margin: "20%", fontSize: "0.8em" }}>
-            2 Sites
-          </p>
+          <p style={{ margin: "20%", fontSize: "0.8em" }}>2 Sites</p>
         </div>
         {/* <div
           className="square"
@@ -425,28 +535,49 @@ const maps = () => {
         </p>
       </Row>{" "}
       {/* Map Image Row */}
-      <Row className="justify-content-center">
-        <div>
-          <img
-            className="square"
-            id="maps-frame"
-            src={splitMap}
-            alt="Ascent Map"
-            style={{
-              paddingLeft: "0.1%",
-              paddingRight: "0.1%",
-              paddingTop: "0.1%",
-              paddingBottom: "0.1%",
-            }}
-          />
-        </div>
-      </Row>{" "}
+      <p style={{ marginBottom: "-27%" }}>`</p>
+      <Row className="justify-content-center" style={{ marginBottom: "24%" }}>
+        <Carousel
+          className="Carousel"
+          style={{
+            marginTop: "0px",
+            marginBottom: "0px",
+          }}
+        >
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={splitMap}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+          <Carousel.Item style={{ marginTop: "5%", marginBottom: "5%" }}>
+            <img
+              className="square"
+              id="maps-frame"
+              src={splitLayout}
+              alt="Haven Map"
+              style={{
+                paddingLeft: "0.1%",
+                paddingRight: "0.1%",
+                paddingTop: "0.1%",
+                paddingBottom: "0.1%",
+              }}
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Row>
       {/* Info Box Row */}
       <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
         <div className="square" id="info-box">
-          <p style={{ color: "white", margin: "20%", fontSize: "0.8em" }}>
-            2 Sites
-          </p>
+          <p style={{ margin: "20%", fontSize: "0.8em" }}>2 Sites</p>
         </div>
         {/* <div
           className="square"
@@ -524,9 +655,7 @@ const maps = () => {
       {/* Info Box Row */}
       <Row className="justify-content-center" style={{ marginTop: "1.2%" }}>
         <div className="square" id="info-box">
-          <p style={{ color: "white", margin: "20%", fontSize: "0.8em" }}>
-            2 Sites
-          </p>
+          <p style={{ margin: "20%", fontSize: "0.8em" }}>2 Sites</p>
         </div>
         {/* <div
           className="square"
@@ -581,7 +710,7 @@ const maps = () => {
           id="teleport"
           className="title1"
           href="/maps"
-          style={{ fontSize: "1.5em" }}
+          style={{ fontSize: "1.5em", maxWidth: "97%" }}
         >
           Teleport
         </a>
